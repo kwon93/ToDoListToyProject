8420993b170ef609df3125218ffd6c603406fe0e
Ben <shb0801@naver.com>
2023-08-09 14:51:43 +0900

    '09August2023' Initialization

---
+++ src/main/java/green/guemjjoki/GuemjjokiApplication.java
@@ -0,0 +1,13 @@
+package green.guemjjoki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuemjjokiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuemjjokiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/green/guemjjoki/GuemjjokiApplicationTests.java
@@ -0,0 +1,13 @@
+package green.guemjjoki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuemjjokiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
